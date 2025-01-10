b9002ed3e6f51c2aef4697d6220abbc67f928fed
AlexeySH8 <shevchenkowe@gmail.com>
2025-01-10 14:52:10 +0500

    Create LongestCommonSubsequenceCalculator.cs

---
+++ Динамическое программирование/Diff Tool/LongestCommonSubsequenceCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Antiplagiarism;
+
+public static class LongestCommonSubsequenceCalculator
+{
+    public static List<string> Calculate(List<string> first, List<string> second)
+    {
+        var opt = CreateOptimizationTable(first, second);
+        return RestoreAnswer(opt, first, second);
+    }
+
+    private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
+    {
+        var opt = new int[first.Count + 1, second.Count + 1];
+        for (int i = first.Count - 1; i >= 0; i--)
+            for (int j = second.Count - 1; j >= 0; j--)
+                if (TokenDistanceCalculator.GetTokenDistance(first[i], second[j]) == 0)
+                    opt[i, j] = 1 + opt[i + 1, j + 1];
+                else
+                    opt[i, j] = Math.Max(opt[i + 1, j], opt[i, j + 1]);
+        return opt;
+    }
+
+    private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
+    {
+        var result = new List<string>();
+        int i = 0, j = 0;
+        while (i < first.Count && j < second.Count)
+        {
+            if (TokenDistanceCalculator.GetTokenDistance(first[i], second[j]) == 0)
+            {
+                result.Add(first[i]);
+                i++;
+                j++;
+            }
+            else if (opt[i + 1, j] > opt[i, j + 1])
+                i++;
+            else
+                j++;
+        }
+        return result;
+    }
+}
